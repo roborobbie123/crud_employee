e2dae451db93a0b75258639a2ed32a72124c1355
Robert Barrett <roborobbie123@gmail.com>
2025-02-03 16:17:56 -0500

    added all crud operations to restcontroller

--- src/main/java/com/robb/crudemployee/Controller/EmployeeRestController.java
+++ src/main/java/com/robb/crudemployee/Controller/EmployeeRestController.java
@@ -3,10 +3,7 @@ package com.robb.crudemployee.Controller;
 import com.robb.crudemployee.DAO.EmployeeDAO;
 import com.robb.crudemployee.Entity.Employee;
 import com.robb.crudemployee.Service.EmployeeServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +19,50 @@ public class EmployeeRestController {
 
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
+
         return employeeService.findAll();
     }
 
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
-       return employeeService.findById(id);
+
+       Employee employee = employeeService.findById(id);
+
+       if(employee == null) {
+           throw new RuntimeException("Employee with id " + id + " not found");
+       }
+       return employee;
     }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+       // set id to 0 to force a new save, rather than an update
+       employee.setId(0);
+
+       Employee dbEmployee = employeeService.save(employee);
+
+       return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+       Employee dbEmployee = employeeService.save(employee);
+
+       return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+
+       if(employeeService.findById(id) == null) {
+           throw new RuntimeException("Employee with id " + id + " not found");
+       }
+
+       employeeService.deleteById(id);
+    }
+
 
 
 }
