00f49a040a5d12f96b42503427ac825c637c4faa
Robert Barrett <roborobbie123@gmail.com>
2025-02-03 15:25:27 -0500

    created employee entity

---
+++ src/main/java/com/robb/crudemployee/Controller/EmployeeRestController.java
@@ -0,0 +1,11 @@
+package com.robb.crudemployee.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+
+}
